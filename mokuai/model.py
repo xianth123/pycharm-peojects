ccd1059e6835bac539da7d6b11f7c99961194545
xianth <xianth123@qq.com>
2016-10-05 16:40:50 +0800

    the help of model's using method

---
+++ mokuai/model.py
@@ -0,0 +1,17 @@
+import flask, pprint
+import datetime
+import flask_bootstrap, flask_moment
+
+# pprint.pprint(dir(flask))
+# print help(datetime)
+# pprint.pprint(dir(flask_bootstrap))
+# print help(flask_bootstrap.url_for)
+# print flask_bootstrap.Bootstrap.__doc__
+# pprint.pprint(help(flask.Flask))
+a = help(flask.Flask)
+# print type(a)
+# help(flask.Flask)
+# with open('Flask_help.txt', 'w') as f:
+#     for i in a:
+#         f.write(i)
+
